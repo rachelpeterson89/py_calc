cb4330830054307d030cd5640fe6547b487c86b4
Rachel Peterson <rachelp5446@gmail.com>
2021-04-15 16:30:05 -0500

    multiply, divide and subtract

--- calc.py
+++ calc.py
@@ -18,13 +18,51 @@ def btn_clear():
 def btn_add():
     num_one = e.get()
     global num_1
+    global math
+    math = "addition"
+    num_1 = int(num_one)
+    e.delete(0, END)
+
+def btn_subtract():
+    num_one = e.get()
+    global num_1
+    global math
+    math = "subtraction"
+    num_1 = int(num_one)
+    e.delete(0, END)
+
+def btn_multiply():
+    num_one = e.get()
+    global num_1
+    global math
+    math = "multiplication"
+    num_1 = int(num_one)
+    e.delete(0, END)
+
+def btn_divide():
+    num_one = e.get()
+    global num_1
+    global math
+    math = "division"
     num_1 = int(num_one)
     e.delete(0, END)
 
 def btn_equal():
     num_two = e.get()
     e.delete(0, END)
-    e.insert(0, num_1 + int(num_two))
+
+    if math == "addition":
+        e.insert(0, num_1 + int(num_two))
+    if math == "subtraction":
+        e.insert(0, num_1 - int(num_two))
+    if math == "multiplication":
+        e.insert(0, num_1 * int(num_two))
+    if math == "division":
+        e.insert(0, num_1 / int(num_two))
+
+def btn_decimal():
+    # Code here
+    return
 
 
 # Define buttons on calculator
@@ -38,7 +76,12 @@ btn7 = Button(root, text="7", padx=40, pady=20, command=lambda: btn_click(7))
 btn8 = Button(root, text="8", padx=40, pady=20, command=lambda: btn_click(8))
 btn9 = Button(root, text="9", padx=40, pady=20, command=lambda: btn_click(9))
 btn0 = Button(root, text="0", padx=40, pady=20, command=lambda: btn_click(0))
+
 btn_add = Button(root, text="+", padx=40, pady=20, command=btn_add)
+btn_subtract = Button(root, text="-", padx=40, pady=20, command=btn_subtract)
+btn_multiply = Button(root, text="*", padx=40, pady=20, command=btn_multiply)
+btn_divide = Button(root, text="/", padx=40, pady=20, command=btn_divide)
+btn_decimal = Button(root, text=".", padx=40, pady=20, command=btn_click("."))
 btn_equal = Button(root, text="=", padx=40, pady=20, command=btn_equal)
 btn_clear = Button(root, text="C", padx=40, pady=20, command=btn_clear)
 
@@ -59,6 +102,11 @@ btn9.grid(row=3, column=2)
 btn0.grid(row=6, column=1)
 
 btn_add.grid(row=5, column=3)
+btn_subtract.grid(row=4, column=3)
+btn_multiply.grid(row=3, column=3)
+btn_divide.grid(row=2, column=3)
+
+btn_decimal.grid(row=6, column=2)
 btn_equal.grid(row=6, column=3)
 btn_clear.grid(row=1, column=3)
 
